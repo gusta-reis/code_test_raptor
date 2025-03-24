b1f9f11b6a7a4e994f29aa87f264e7af51c78e49
Gustavo Reis <gustavophelipe_reis@hotmail.com>
2025-03-24 07:28:56 -0400

    Workspaces Code Test Raptor
    Workspaces do hello_world_raptor e do doom-mcx

---
+++ build/zephyr/zephyr.dts.d
@@ -0,0 +1,18 @@
+empty_file.o: \
+ C:/Users/gusta/OneDrive/Documents/GitHub/zephyr/misc/empty_file.c \
+ C:/Users/gusta/OneDrive/Documents/GitHub/zephyr/boards/qemu/cortex_a53/qemu_cortex_a53.dts \
+ C:/Users/gusta/OneDrive/Documents/GitHub/zephyr/dts/arm64/qemu/qemu-virt-a53.dtsi \
+ C:/Users/gusta/OneDrive/Documents/GitHub/zephyr/dts/common/mem.h \
+ C:/Users/gusta/OneDrive/Documents/GitHub/zephyr/dts/arm64/armv8-a.dtsi \
+ C:/Users/gusta/OneDrive/Documents/GitHub/zephyr/dts/common/skeleton.dtsi \
+ C:/Users/gusta/OneDrive/Documents/GitHub/zephyr/include/zephyr/dt-bindings/interrupt-controller/arm-gic.h \
+ C:/Users/gusta/OneDrive/Documents/GitHub/zephyr/include/zephyr/dt-bindings/dt-util.h \
+ C:/Users/gusta/OneDrive/Documents/GitHub/zephyr/include/zephyr/sys/util_macro.h \
+ C:/Users/gusta/OneDrive/Documents/GitHub/zephyr/include/zephyr/sys/util_internal.h \
+ C:/Users/gusta/OneDrive/Documents/GitHub/zephyr/include/zephyr/sys/util_loops.h \
+ C:/Users/gusta/OneDrive/Documents/GitHub/zephyr/include/zephyr/sys/util_listify.h \
+ C:/Users/gusta/OneDrive/Documents/GitHub/zephyr/include/zephyr/sys/util_internal_is_eq.h \
+ C:/Users/gusta/OneDrive/Documents/GitHub/zephyr/include/zephyr/sys/util_internal_util_inc.h \
+ C:/Users/gusta/OneDrive/Documents/GitHub/zephyr/include/zephyr/sys/util_internal_util_dec.h \
+ C:/Users/gusta/OneDrive/Documents/GitHub/zephyr/include/zephyr/sys/util_internal_util_x2.h \
+ C:/Users/gusta/OneDrive/Documents/GitHub/zephyr/include/zephyr/dt-bindings/pcie/pcie.h
